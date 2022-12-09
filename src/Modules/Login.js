f3c2338c0d5cf802ebe581c469d711817f86fc6b
Minji Kim <110662771+sloane323@users.noreply.github.com>
2022-12-09 21:26:45 +0900

    new

---
+++ src/Modules/Login.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  isLoggedIn: false,
+  currentUser: localStorage.getItem("currentUser")
+    ? localStorage.getItem("currentUser")
+    : "비회원",
+};
+const login = createSlice({
+  name: "login",
+  initialState,
+  reducers: {
+    LOGIN: (state, action) => {
+      state.isLoggedIn = true;
+      localStorage.setItem("currentUser", action.payload);
+    },
+    LOGOUT: (state) => {
+      state.isLoggedIn = false;
+      localStorage.removeItem("currentUser");
+    },
+  },
+});
+export const { LOGIN, LOGOUT } = login.actions;
+export default login.reducer;
